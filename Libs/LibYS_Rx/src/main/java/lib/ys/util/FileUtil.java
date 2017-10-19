1b2ff125b7dc9cafa5b296298cb57a83db6efe41
805717671@qq.com <805717671@qq.com>
2017-10-19 10:13:19 +0800

    1.直播音频加入上传同步指令 2.录播音频上传逻辑完善

--- Libs/LibYS_Rx/src/main/java/lib/ys/util/FileUtil.java
+++ Libs/LibYS_Rx/src/main/java/lib/ys/util/FileUtil.java
@@ -1,7 +1,10 @@
 package lib.ys.util;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -258,5 +261,73 @@ public class FileUtil {
             }
         }
         return size;
+    }
+
+    /**
+     * 获得指定文件的byte数组
+     *
+     * @param filePath
+     * @return
+     */
+    public static byte[] fileToBytes(String filePath) {
+        byte[] buffer = null;
+        try {
+            File file = new File(filePath);
+            FileInputStream fis = new FileInputStream(file);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream(BUFFER_SIZE);
+            byte[] b = new byte[BUFFER_SIZE];
+            int n;
+            while ((n = fis.read(b)) != -1) {
+                bos.write(b, 0, n);
+            }
+            fis.close();
+            bos.close();
+            buffer = bos.toByteArray();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return buffer;
+    }
+
+    /**
+     * 根据byte数组，生成文件
+     *
+     * @param bfile
+     * @param filePath
+     * @param fileName
+     */
+    public static void bytesToFile(byte[] bfile, String filePath,String fileName) {
+        BufferedOutputStream bos = null;
+        FileOutputStream fos = null;
+        File file = null;
+        try {
+            File dir = new File(filePath);
+            if(!dir.exists()&&dir.isDirectory()){//判断文件目录是否存在
+                dir.mkdirs();
+            }
+            file = new File(filePath+"\\"+fileName);
+            fos = new FileOutputStream(file);
+            bos = new BufferedOutputStream(fos);
+            bos.write(bfile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
     }
 }
