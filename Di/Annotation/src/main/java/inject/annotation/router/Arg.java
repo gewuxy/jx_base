dfaad0e06aa42b28433f49fdcd42be90af0e3188
forgetman@qq.com <forget850821>
2017-10-24 14:47:45 +0800

    整理bind模块

--- Di/Annotation/src/main/java/inject/annotation/router/Arg.java
+++ Di/Annotation/src/main/java/inject/annotation/router/Arg.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
  * @since 2017/8/1
  */
 @Target(ElementType.FIELD)
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.CLASS)
 public @interface Arg {
     /**
      * 替换的key
