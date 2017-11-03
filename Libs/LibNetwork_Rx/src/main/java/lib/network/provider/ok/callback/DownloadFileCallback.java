336b02d2b2702e07d17f1194e80a7e65f5815ff5
forgetman@qq.com <forget850821>
2017-11-03 16:19:18 +0800

    加入支持大文件下载

--- Libs/LibNetwork_Rx/src/main/java/lib/network/provider/ok/callback/DownloadFileCallback.java
+++ Libs/LibNetwork_Rx/src/main/java/lib/network/provider/ok/callback/DownloadFileCallback.java
@@ -1,8 +1,9 @@
 package lib.network.provider.ok.callback;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
 
 import lib.network.NetworkLog;
 import lib.network.model.NetworkErrorBuilder;
@@ -13,6 +14,7 @@ import okhttp3.Response;
 
 /**
  * 返回的bytes直接存为文件
+ * FIXME: 暂不支持断点续传
  *
  * @auther yuansui
  * @since 2017/6/11
@@ -34,7 +36,7 @@ public class DownloadFileCallback extends OkCallback {
     public void onResponse(Call call, Response response) throws IOException {
         Integer id = (Integer) call.request().tag();
 
-        FileOutputStream os = null;
+        RandomAccessFile raf = null;
         try {
             File dir = new File(mFileDir);
             if (!dir.exists()) {
@@ -45,8 +47,21 @@ public class DownloadFileCallback extends OkCallback {
                 }
             }
 
-            os = new FileOutputStream(new File(mFileDir, mFileName));
-            os.write(response.body().bytes());
+            File targetFile = new File(mFileDir, mFileName);
+            if (targetFile.exists()) {
+                targetFile.delete();
+            }
+
+            InputStream stream = response.body().byteStream();
+            raf = new RandomAccessFile(targetFile, "rw");
+            byte[] bytes = new byte[1024];
+            int len = 0;
+            while ((len = stream.read(bytes)) != -1) {
+                if (call.isCanceled()) {
+                    return;
+                }
+                raf.write(bytes, 0, len);
+            }
 
             NativeListener.inst().onSuccess(id, null, getListener());
         } catch (Exception e) {
@@ -55,12 +70,8 @@ public class DownloadFileCallback extends OkCallback {
                     NetworkErrorBuilder.create().code(id).exception(e).message(e.getMessage()).build(),
                     getListener());
         } finally {
-            if (os != null) {
-                try {
-                    os.close();
-                } catch (IOException e) {
-                    NetworkLog.e(e);
-                }
+            if (raf != null) {
+                raf.close();
             }
         }
     }
