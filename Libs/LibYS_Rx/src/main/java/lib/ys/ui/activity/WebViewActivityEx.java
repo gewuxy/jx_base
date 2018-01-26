3e2138f34d7b00bd9bff9d9517a43884eda7a960
805717671@qq.com <w3991019>
2018-01-26 11:54:44 +0800

    webView释放资源

--- Libs/LibYS_Rx/src/main/java/lib/ys/ui/activity/WebViewActivityEx.java
+++ Libs/LibYS_Rx/src/main/java/lib/ys/ui/activity/WebViewActivityEx.java
@@ -3,6 +3,8 @@ package lib.ys.ui.activity;
 import android.content.Intent;
 import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
@@ -118,5 +120,28 @@ abstract public class WebViewActivityEx extends ActivityEx implements IWebViewHo
     public void onPause() {
         super.onPause();
         mWebView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mWebView != null) {
+
+            // 如果先调用destroy()方法，则会命中if (isDestroyed()) return;这一行代码，需要先onDetachedFromWindow()，再
+            // destory()
+            ViewParent parent = mWebView.getParent();
+            if (parent != null) {
+                ((ViewGroup) parent).removeView(mWebView);
+            }
+
+            mWebView.stopLoading();
+            // 退出时调用此方法，移除绑定的服务，否则某些特定系统会报错
+            mWebView.getSettings().setJavaScriptEnabled(false);
+            mWebView.clearHistory();
+            mWebView.clearView();
+            mWebView.removeAllViews();
+            mWebView.destroy();
+
+        }
+        super.onDestroy();
     }
 }
